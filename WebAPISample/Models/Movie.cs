21833ade6de1c867477f5d5474eaedf4ec4711d8
jjvega86 <jj.vega86@gmail.com>
2020-09-29 13:54:06 -0400

    Added genre to Movie model and seeded database with initial data

--- WebAPISample/Models/Movie.cs
+++ WebAPISample/Models/Movie.cs
@@ -11,5 +11,7 @@ namespace WebAPISample.Models
         public int MovieId { get; set; }
         public string Title { get; set; }
         public string Director { get; set; }
+
+        public string Genre { get; set; }
     }
 }
