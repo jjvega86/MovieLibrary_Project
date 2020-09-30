2d5828cce8a7f44e9cb9c25944b55653d5e9f548
jjvega86 <jj.vega86@gmail.com>
2020-09-30 15:50:32 -0400

    Added table function and element to app.js and index.html

--- Client/app.js
+++ Client/app.js
@@ -1,40 +1,46 @@
-$(function(){
+$(document).ready(function() {
+    // all custom jQuery will go here
 
-    $.get("https://localhost:44325/api/movie/", function(data){
-        console.log(data);
+    $(function(){
 
-        data.map(function(el){
-            $("#Movies").append(`<div> Movie Title: ${el.title} </div>
-            <div> Movie Director: ${el.director}</div>
-            <br>`);
-        })
-    })
-})
+        $.get("https://localhost:44325/api/movie/", function(data){
+            console.log(data);
+    
+            data.map(function(el){
+                $("#tbody").append(`<td class="row-index text-center"> 
+                <p>${el.title}</p></td> <td class="row-index text-center"> 
+                <p>${el.director}</p></td><td class="row-index text-center"> 
+                <p>${el.genre}</p></td>`);
+            })
+        })      
+    });
+    
+    (function($){
+        function processForm( e ){
+            var dict = {
+                Title : this["title"].value,
+                Director: this["director"].value,
+                Genre: this["genre"].value
+            };
+    
+            $.ajax({
+                url: 'https://localhost:44325/api/movie',
+                dataType: 'json',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify(dict),
+                success: function( data, textStatus, jQxhr ){
+                    $('#response pre').html( data );
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log( errorThrown );
+                }
+            });
+    
+            e.preventDefault();
+        }
+    
+        $('#my-form').submit( processForm );
+    })(jQuery);
+});
 
-$(function(){
-    function processForm( e ){
-        var dict = {
-        	Title : this["title"].value,
-            Director: this["director"].value,
-            Genre: this["genre"].value
-        };
-
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
-
-        e.preventDefault();
-    }
-
-    $('#my-form').submit( processForm );
-})(jQuery);
