f77db8b165a9ca4fd1938cecc510af0294109cec
Eric M Hill <ehill111@comcast.net>
2020-10-01 18:59:33 -0400

    Pull

--- Client/app.js
+++ Client/app.js
@@ -12,9 +12,9 @@ $(document).ready(function() {
                 <td><p>${el.title}</p></td> 
                 <td><p>${el.director}</p></td>
                 <td><p>${el.genre}</p></td>
-                /* <td>ADD IMAGE BUTTON</td>
-                <td>EDIT IMAGE BUTTON</td>
-                <td>VIEW IMAGE BUTTON</td> */
+                /* <td>Add Image Button</td>
+                <td>Edit Image Button</td>
+                <td>View Image Button</td> */
 
                 </tr>`);
 
